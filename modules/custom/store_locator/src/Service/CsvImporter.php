abeaa2e44cd4d7b6ae19cb4d38950ac98f6256bd
william <williamprritchard@googlemail.com>
2025-06-19 12:50:11 +0100

    Merge branch 'Store_locator' of https://github.com/willp34/Drupal-10-demo into Store_locator
    # Conflicts:
    #	.gitignore
    #	modules/custom/store_locator/config/install/store_locator.settings.yml

--- modules/custom/store_locator/src/Service/CsvImporter.php
+++ modules/custom/store_locator/src/Service/CsvImporter.php
@@ -31,7 +31,7 @@ class CsvImporter {
 	$this->configFactory = $configFactory; // <---- MISSING LINE
 	  // Load the config at construction time
     $this->googleApiKey = $this->configFactory->get('store_locator.settings')->get('api_key');
-  
+    //remove google  key
   }
 
   public function readCsvIndex($file_path) {
